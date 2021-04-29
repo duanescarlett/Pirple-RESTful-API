62649a77e481caff339ce95b8a50f698ce661800
dos <duanescarlett@gmail.com>
2021-04-28 20:03:00 -0400

    Added streams and request handling

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 // Dependencies
 var http = require('http')
 var url = require('url')
+var StringDecoder = require('string_decoder').StringDecoder
 
 // The server should respond to all request with a string
 var server = http.createServer((req, res) => {
@@ -19,12 +20,32 @@ var server = http.createServer((req, res) => {
 
   // Get the HTTP Method
   var method = req.method.toLowerCase()
+
+  // Get the headers as an object
+  var headers = req.headers
   
-  // Send the response
-  res.end('Hello Nodejs\n')
+  // Get the payload if there is any
+  var decoder = new StringDecoder('utf-8')
+  var buffer = ''
+
+  // On the event (data) compile stream into the var called (buffer)
+  req.on('data', (data) => {
+    buffer += decoder.write(data)
+  })
+
+  req.on('end', () => {
+    buffer += decoder.end()
+    // Send the response
+    res.end('Hello Nodejs\n')
+    // Log the request path
+    console.log('Request received with this payload: ', buffer)
+  })
+
+
   
   // Log the request path
-  console.log('Request received on path: ' + trimmedPath + ' with method: ' + method + ' and with these perameters ', queryStringObject)
+  // console.log('Request received on path: ' + trimmedPath + ' with method: ' + method + ' and with these perameters ', queryStringObject)
+  // console.log('Request received with these headers', headers)
 
 })
 
