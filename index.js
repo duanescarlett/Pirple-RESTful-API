f7ecaeab3a5580b48aa7db874cae700b88943a44
dos <duanescarlett@gmail.com>
2021-07-27 21:20:22 -0400

    fix some nasty bugs

--- index.js
+++ index.js
@@ -3,15 +3,31 @@
 // Dependencies
 var http = require('http')
 var https = require('https')
-// var url = require('url')
+var url = require('url')
 var StringDecoder = require('string_decoder').StringDecoder
 var config = require('./lib/config')
 var fs = require('fs')
 const handlers = require('./lib/handlers')
 const helpers = require('./lib/helpers')
-
+const _data = require('./lib/data')
 // The server should respond to all request with a string
 
+// _data.delete('test', 'branNewFile', (err) => {
+//   console.log('this was the error', err)
+// })
+
+// _data.update('test', 'newFile', {'fizz': 'buzz'}, (err) => {
+//   console.log('this was the error', err)
+// })
+
+// _data.read('test', 'newFile', (err, data) => {
+//   console.log('this was the error', err)
+//   console.log('this was the data: ', data)
+// })
+
+// _data.create('test', 'newFile', {'foo' : 'bar'}, (err) => {
+//   console.log('this was the error', err)
+// })
 // Instanciate the HTTP server
 var httpServer = http.createServer((req, res) => {
   unifiedServer(req, res)
@@ -41,8 +57,8 @@ httpsServer.listen(config.httpsPort, () => {
 // All the server logic for both the http and https server
 var unifiedServer = (req, res) => {
   // Get the url and parse it
-  // var parsedUrl = url.parse(req.url, true)
-  var parsedUrl = new URL(`https://localhost:3001${req.url}`)
+  var parsedUrl = url.parse(req.url, true)
+  // var parsedUrl = new URL(`https://localhost:3001${req.url}`)
   // Get the path
   var path = parsedUrl.pathname
   console.log("Path: ", path)
@@ -75,7 +91,7 @@ var unifiedServer = (req, res) => {
 
     // choose the handler that this request goes to
     // If one is not found use the notFound handler
-    var chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound
+    var chosenHandler = typeof(router[trimmedPath]) != 'undefined' ? router[trimmedPath] : handlers.notFound
 
     // Construct the data object to send to the handler
     var data = {
@@ -86,14 +102,14 @@ var unifiedServer = (req, res) => {
       'payload': helpers.parseJsonToObject(buffer)
     }
     
-    10:03
+    // 10:03
     // Route the request to the handler specified in the router
     chosenHandler = (data, (statusCode, payload) => {
       // Use the status code called back by the handler, or default
       statusCode = typeof(statusCode) === 'number' ? statusCode : 200
 
       // Use the payload called back by the handler, or default to an empty object
-      payload = typeof(payload) === 'object' ? payload : {data}
+      payload = typeof(payload) === 'object' ? payload : {}
 
       // Convert the payload to a string 
       var payloadString = JSON.stringify(payload)
@@ -102,12 +118,6 @@ var unifiedServer = (req, res) => {
       res.setHeader('Content-Type', 'application/json')
       res.writeHead(statusCode)
       res.end(payloadString)
-
-      // handlers.users(data, (err, statusCode) => {
-      //   if (err) {
-      //     console.log(err)
-      //   }
-      // })
 
       // Log the request path 
       console.log('Returning this response: ', statusCode, payloadString) 
